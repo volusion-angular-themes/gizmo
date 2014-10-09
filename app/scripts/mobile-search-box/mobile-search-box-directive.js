ec78439a6986cd51b92cc0f6a48d17ee767db730
Tyler Tadej <tylertadej@users.noreply.github.com>
2014-10-09 10:48:23 -0700

    Add custom directive for mobile search box

---
+++ app/scripts/mobile-search-box/mobile-search-box-directive.js
@@ -0,0 +1,56 @@
+'use strict';
+/**
+ * @ngdoc directive
+ * @name mobileSearchBox
+ *
+ * @description
+ * Custom directive for mobile search box in Gizmo
+ *
+ * @restrict AE
+ *
+ * @scope
+ *
+ * @usage
+ * <div mobile-search-box></div>
+ *
+ * */
+
+
+angular.module('gizmoApp')
+		.directive('mobileSearchBox', ['vnSearchManager', function (vnSearchManager) {
+
+			return {
+				templateUrl: 'scripts/mobile-search-box/mobile-search-box.tpl.html',
+				restrict   : 'AE',
+				replace    : true,
+				scope      : {
+					searchTerm: '=',
+					showSearch: '='
+				},
+				link       : function postLink(scope, element, attrs) {
+					element.bind('click', function () {
+						element.find('input').focus();
+					});
+
+					scope.searchTerm = scope.searchTerm || vnSearchManager.getSearchText();
+					scope.allowCollapse = attrs.allowCollapse && !!(JSON.parse(attrs.allowCollapse));
+
+					scope.doSearch = function () {
+						vnSearchManager.updateSearch(scope.searchTerm);
+						scope.showSearch = false;
+						if (scope.showSearch) {
+							scope.showSearch = false;
+						}
+					};
+
+					scope.$watch(
+							function () {
+								return vnSearchManager.getSearchText();
+							},
+							function (searchText) {
+								scope.searchTerm = searchText || '';
+							}, true
+					);
+				}
+			};
+		}]);
